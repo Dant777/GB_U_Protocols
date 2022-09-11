0d1f433d21d4b30a7e1cceabde5542c950ba9f65
Dant777 <47300885+Dant777@users.noreply.github.com>
2022-09-11 07:03:09 +0300

    init

---
+++ PumpService/PumpService/IPumpService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpService
+{
+    [ServiceContract]
+    public interface IPumpService
+    {
+        [OperationContract]
+        void RunScript();
+
+        [OperationContract]
+        void UpdateAndCompileScript(string fileName);
+    }
+}
